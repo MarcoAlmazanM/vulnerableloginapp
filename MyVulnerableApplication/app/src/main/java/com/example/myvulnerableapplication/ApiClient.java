5ec3c0d5747e44d9669fa0e5d4799bfc5d8f81e2
Marco <markosayajin@hotmail.com>
2022-11-21 19:37:30 -0600

    VulnerableApp

--- MyVulnerableApplication/app/src/main/java/com/example/myvulnerableapplication/ApiClient.java
+++ MyVulnerableApplication/app/src/main/java/com/example/myvulnerableapplication/ApiClient.java
@@ -12,7 +12,7 @@ public class ApiClient {
         OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build();
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl("http://192.168.56.1/ServerApp/")
+                .baseUrl("http://192.168.56.1/Server/")
                 .client(okHttpClient)
                 .build();
         return retrofit;
